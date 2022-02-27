93e758e6f26d0ec45c3fd021b5d1a9982044adf2
Carlos Antonio Suarez Quintero <Carlos.SuarezQ@carvajal.com>
2022-02-27 11:31:50 -0500

    Feat<Firebase> : Añado firebase para guardar las imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,14 @@ export const environment = {
   production: true,
   name: 'prod',
   firebase: {
-        config : {}
+        config : {
+          apiKey: "AIzaSyCey87gttuxvhgWwdBfYn63iqMysHo3in0",
+          authDomain: "storeapp-c5f8a.firebaseapp.com",
+          projectId: "storeapp-c5f8a",
+          storageBucket: "storeapp-c5f8a.appspot.com",
+          messagingSenderId: "942329364102",
+          appId: "1:942329364102:web:a691922032cef2296b9b07"
+        }
   },
   actionCodeSettings: {
     url: 'https://ecommerce-e4e6c.web.app/profile/new',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,14 @@ export const environment = {
   production: false,
   name: 'default',
   firebase: {
-        config : {}
+        config : {
+          apiKey: "AIzaSyCey87gttuxvhgWwdBfYn63iqMysHo3in0",
+          authDomain: "storeapp-c5f8a.firebaseapp.com",
+          projectId: "storeapp-c5f8a",
+          storageBucket: "storeapp-c5f8a.appspot.com",
+          messagingSenderId: "942329364102",
+          appId: "1:942329364102:web:a691922032cef2296b9b07"
+        }
   },
   actionCodeSettings: {
     url: 'http://localhost:5200/profile/new',
